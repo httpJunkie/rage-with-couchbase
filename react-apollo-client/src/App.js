4fefb079656d6475253cc1be4c88ea1fedbc58d4
Eric S. Bishard <e.bishard@gmail.com>
2019-12-02 10:12:52 -0800

    Add Menu to App Comment any context related code out for now (will add context api soon)

--- react-apollo-client/src/App.js
+++ react-apollo-client/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from './components/routes/home';
 import Airlines from './components/routes/airlines';
+
+import Menu from './components/partial/menu';
 import Error404 from './components/partial/error-404';
 
 const App = () => {
   return (
     <div className="app-container pad-one">
       <BrowserRouter>
+        <Menu />
         <Switch>
         <Route exact path={["/home", "/"]}
             render={(props) => <Home {...props} />}
